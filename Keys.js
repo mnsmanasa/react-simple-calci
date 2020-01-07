d7d96e065bca561c59a5ce89a32cca16648a810b
mnsmanasa <saimanasa1502@gmail.com>
2020-01-07 11:55:27 +0530

    2 files modified

--- Keys.js
+++ Keys.js
@@ -8,6 +8,10 @@ class Keys extends Component {
     };
   }
 
+  calciClick = (num)=> {
+    console.log(num)
+  }
+
   render() {
     return (
       <form>
@@ -22,29 +26,29 @@ class Keys extends Component {
             </td>
           </tr>
           <tr>
-            <td><button>1</button></td>
-            <td><button>2</button></td>
-            <td><button>3</button></td>
-            <td><button>*</button></td>
-            <td rowspan="4" class="h-100"><button>=</button></td>
+            <td onClick={()=>this.calciClick(1)}>1</td>
+            <td onClick={()=>this.calciClick(2)}>2</td>
+            <td onClick={()=>this.calciClick(3)}>3</td>
+            <td onClick={()=>this.calciClick('*')}>*</td>
+            <td rowspan="4" className="h-100">=</td>
           </tr>
           <tr>
-            <td><button>4</button></td>
-            <td><button>5</button></td>
-            <td><button>6</button></td>
-            <td><button>/</button></td>
+            <td onClick={()=>this.calciClick(4)}>4</td>
+            <td onClick={()=>this.calciClick(5)}>5</td>
+            <td onClick={()=>this.calciClick(6)}>6</td>
+            <td onClick={()=>this.calciClick('/')}>/</td>
           </tr>
           <tr>
-            <td><button>7</button></td>
-            <td><button>8</button></td>
-            <td><button>9</button></td>
-            <td><button>-</button></td>
+            <td onClick={()=>this.calciClick(7)}>7</td>
+            <td onClick={()=>this.calciClick(8)}>8</td>
+            <td onClick={()=>this.calciClick(9)}>9</td>
+            <td onClick={()=>this.calciClick('-')}>-</td>
           </tr>
           <tr>
-            <td><button>.</button></td>
-            <td><button>0</button></td>
-            <td><button>C</button></td>
-            <td><button>+</button></td>
+            <td onClick={()=>this.calciClick('.')}>.</td>
+            <td onClick={()=>this.calciClick(0)}>0</td>
+            <td onClick={()=>this.calciClick(C)}>C</td>
+            <td onClick={()=>this.calciClick('+')}>+</td>
           </tr>
         </table>
       </form>
