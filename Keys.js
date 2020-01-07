c3ef0845f57fdf235ebdd9254507411e84d1c0dd
mnsmanasa <saimanasa1502@gmail.com>
2020-01-07 14:09:30 +0530

    2 files modified

--- Keys.js
+++ Keys.js
@@ -21,58 +21,56 @@ class Keys extends Component {
   calculateResult = () => {
     this.setState({
       calciValue: eval(this.state.calciValue),
-      array:[eval(this.state.calciValue)]
-    })
+      array: [eval(this.state.calciValue)]
+    });
   };
 
-  clearCalci =()=>{
-    this.setState({calciValue: 0, array: []})
-  }
+  clearCalci = () => {
+    this.setState({ calciValue: 0, array: [] });
+  };
 
   render() {
     return (
-      <form>
-        <table cellSpacing="0">
-          <tbody>
-            <tr>
-              <td colSpan="5">
-                <input
-                  type="text"
-                  name="calcinput"
-                  value={this.state.calciValue}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td onClick={() => this.calciClick("1")}>1</td>
-              <td onClick={() => this.calciClick("2")}>2</td>
-              <td onClick={() => this.calciClick("3")}>3</td>
-              <td onClick={() => this.calciClick("*")}>*</td>
-              <td rowSpan="4" className="h-100" onClick={this.calculateResult}>
-                =
-              </td>
-            </tr>
-            <tr>
-              <td onClick={() => this.calciClick("4")}>4</td>
-              <td onClick={() => this.calciClick("5")}>5</td>
-              <td onClick={() => this.calciClick("6")}>6</td>
-              <td onClick={() => this.calciClick("/")}>/</td>
-            </tr>
-            <tr>
-              <td onClick={() => this.calciClick("7")}>7</td>
-              <td onClick={() => this.calciClick("8")}>8</td>
-              <td onClick={() => this.calciClick("9")}>9</td>
-              <td onClick={() => this.calciClick("-")}>-</td>
-            </tr>
-            <tr>
-              <td onClick={() => this.calciClick(".")}>.</td>
-              <td onClick={() => this.calciClick("0")}>0</td>
-              <td onClick={this.clearCalci}>C</td>
-              <td onClick={() => this.calciClick("+")}>+</td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
+      <table cellSpacing="0">
+        <tbody>
+          <tr>
+            <td colSpan="5">
+              <input
+                type="text"
+                name="calcinput"
+                value={this.state.calciValue}
+              />
+            </td>
+          </tr>
+          <tr>
+            <td onClick={() => this.calciClick("1")}>1</td>
+            <td onClick={() => this.calciClick("2")}>2</td>
+            <td onClick={() => this.calciClick("3")}>3</td>
+            <td onClick={() => this.calciClick("*")}>*</td>
+            <td rowSpan="4" className="h-100" onClick={this.calculateResult}>
+              =
+            </td>
+          </tr>
+          <tr>
+            <td onClick={() => this.calciClick("4")}>4</td>
+            <td onClick={() => this.calciClick("5")}>5</td>
+            <td onClick={() => this.calciClick("6")}>6</td>
+            <td onClick={() => this.calciClick("/")}>/</td>
+          </tr>
+          <tr>
+            <td onClick={() => this.calciClick("7")}>7</td>
+            <td onClick={() => this.calciClick("8")}>8</td>
+            <td onClick={() => this.calciClick("9")}>9</td>
+            <td onClick={() => this.calciClick("-")}>-</td>
+          </tr>
+          <tr>
+            <td onClick={() => this.calciClick(".")}>.</td>
+            <td onClick={() => this.calciClick("0")}>0</td>
+            <td onClick={this.clearCalci}>C</td>
+            <td onClick={() => this.calciClick("+")}>+</td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 }
